ced9070167564befc3f323b4a3f62499504a5c19
qwertycho <tychovopstal@gmail.com>
2021-12-09 16:09:22 +0100

    functies verbeterd

--- js/winkeldmandje.js
+++ js/winkeldmandje.js
@@ -1,30 +1,24 @@
 const twok = {naam: "Lamy 2000", prijs: 200};
-
 const safari = {naam: "safari", prijs: 20};
 const logo = {naam: "logo", prijs: 30};
-
 const notebook1 = {naam: "notebook 1", prijs: 20};
-
 const notebook2 = {naam: "notebook 2", prijs: 20};
-
 const notebook3 = {naam: "notebook 3", prijs: 20};
-
 const inkt1 = {naam: "inkt1", prijs: 10};
-
 const inkt2 = {naam: "inkt2", prijs: 10};
-
 const inkt3 = {naam: "inkt3", prijs: 10};
-
 
 const winkelmand = {inhoud: "", totaal: 0}
 
-document.getElementById("2k").addEventListener("click", cart);
-document.getElementById("safari").addEventListener("click", cart)
-document.getElementById("logo").addEventListener("click", cart)
-
+// voegt listenerEvents toe aan alle koop knoppen
+let buttons = document.querySelectorAll(".koop")
+for(let i = 0; i < buttons.length; i++ ){
+    buttons[i].addEventListener("click", cart)
+}
 
 
 function cart(e){
+    // 
     let item = eval(e.target.id)
     winkelmand.inhoud += item.naam
     winkelmand.totaal +=  item.prijs
